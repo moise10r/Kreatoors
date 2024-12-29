16ff9584f2064e3cefe5facbe7a17cfba16668d8
moise10r <moiserushanika2006@gmail.com>
2024-12-29 15:28:14 +0200

    Create contentPreferences list.

--- components/Collaboration.jsx
+++ components/Collaboration.jsx
@@ -2,20 +2,35 @@ import Image from "next/image";
 import { Checkbox, Textarea } from "@material-tailwind/react";
 
 export default function Collaboration() {
-    return (
-        <div className="overscroll-contain">
-            <h1 className="font-semibold mb-6 text-[24px]">Define Your Content Preferences</h1>
-            <p className="mb-6 w-5/6 text-[16px]">What type of content do you want to focus on?</p>
-            <div className="mb-4">
-                <Checkbox id="ripple-on" label="Increase Brand Visibility" ripple={true} />
-                <Checkbox id="ripple-on" label="Attract New Clients" ripple={true} />
-                <Checkbox id="ripple-on" label="Engage Employees" ripple={true} />
-                <Checkbox id="ripple-on" label="Build Thoughtful Leadership" ripple={true} />
-                <Checkbox id="ripple-on" label="Promote Company Culture" ripple={true} />
-                <Checkbox id="ripple-on" label="Educate and Train" ripple={true} />
-                <Checkbox id="ripple-on" label="General Content Creation" ripple={true} />
-                <Checkbox id="ripple-on" label="Improve Internal Communication" ripple={true} />
-            </div>
-        </div>
-    );
+  const contentPreferences = [
+    "Increase Brand Visibility",
+    "Attract New Clients",
+    "Engage Employees",
+    "Build Thoughtful Leadership",
+    "Promote Company Culture",
+    "Educate and Train",
+    "General Content Creation",
+    "Improve Internal Communication",
+  ];
+
+  return (
+    <div className="overscroll-contain">
+      <h1 className="font-semibold mb-6 text-[24px]">
+        Define Your Content Preferences
+      </h1>
+      <p className="mb-6 w-5/6 text-[16px]">
+        What type of content do you want to focus on?
+      </p>
+      <div className="mb-4">
+        {contentPreferences.map((preference, index) => (
+          <Checkbox
+            key={index}
+            id={`preference-${index}`}
+            label={preference}
+            ripple={true}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
